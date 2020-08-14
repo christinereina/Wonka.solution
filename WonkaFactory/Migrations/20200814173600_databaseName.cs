707bdc88638ebbb1d8a7c69029e7c73f99fd58be
blacchoy@gmail.com <blacchoy@gmail.com>
2020-08-14 10:36:15 -0700

    correct database name for migration

---
+++ WonkaFactory/Migrations/20200814173600_databaseName.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WonkaFactory.Migrations
+{
+    public partial class databaseName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
