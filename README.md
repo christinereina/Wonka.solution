a0521bbd153cae3204e148d7a607616baf07632d
blacchoy@gmail.com <blacchoy@gmail.com>
2020-08-14 12:30:39 -0700

    add all views for Flavors

--- README.md
+++ README.md
@@ -38,28 +38,6 @@ dotnet restore
 dotnet build
 dotnet run
 ```
-### Installing and Configuring MySQL
-
-1. Start by downloading the MySQL Community Server .dmg file [HERE](https://dev.mysql.com/downloads/file/?id=484914). Click the download icon. Use the *No thanks, just start my download link*.
-2. Follow along with the Install, once on the configuation make sure you:
-- Use Legacy Password Encryption.
-- Set a unique password
-3. Once finished, open your terminal and enter the command echo `export PATH="/usr/local/mysql/bin:$PATH"' >> ~/.bash_profile`. This will save this path in .bash_profile, which is where our terminal is configured.
-4. Next, download the MySQL Workbench .dmg file [HERE](https://dev.mysql.com/downloads/file/?id=484391). Again, use the *No thanks, just start my download link*.
-5.Install MySQL Workbench to Applications folder.
-
-### Recreating the Local Database
-
-1. Open **MySQL Workbench**, select Local 3306 and enter your password
-2. Use this query selection to create the database:
-```
-CREATE DATABASE `christine_augustine`; 
-
-USE `christine_augustine`;
-
-```
-3. Once it connects, you should be all set!
-
 <h5 align = "right">
 
 [see more detailed instructions for windows/mac](https://www.learnhowtoprogram.com/c-and-net/getting-started-with-c/installing-and-configuring-mysql) 
--- WonkaFactory/Controllers/FlavorsController.cs
+++ WonkaFactory/Controllers/FlavorsController.cs
@@ -29,5 +29,83 @@ namespace Factory.Controllers
       return View(model);
     }
 
+     public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisFlavor= _db.Flavors
+          .Include(flavor => flavor.Treats)
+          .ThenInclude(join => join.Treat)
+          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    // public ActionResult AddRecipe(int id)
+    // {
+    //   var thisCategory = _db.Categories.FirstOrDefault(categories => categories.CategoryId == id);
+    //   ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Description");
+    //   return View(thisCategory);
+    // }
+
+    // [HttpPost]
+    // public ActionResult AddRecipe(Category category, int RecipeId)
+    // {
+    //   if (RecipeId != 0)
+    //   {
+    //     _db.CategoryRecipe.Add(new CategoryRecipe() { RecipeId = RecipeId, CategoryId = category.CategoryId });
+    //   }
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
+
+    public ActionResult Delete(int id)
+    {
+      var thisFlavor  = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor= _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteRecipe(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
