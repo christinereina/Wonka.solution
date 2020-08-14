0173069500ca386a67dae4de1659b4671609bce8
blacchoy@gmail.com <blacchoy@gmail.com>
2020-08-14 12:00:07 -0700

    add controller and view for Flavor index

--- WonkaFactory/Controllers/FlavorsController.cs
+++ WonkaFactory/Controllers/FlavorsController.cs
@@ -14,6 +14,20 @@ namespace Factory.Controllers
   [Authorize]
   public class FlavorsController : Controller
   {
+    private readonly FactoryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public FlavorsController(UserManager<ApplicationUser> userManager, FactoryContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
 
   }
 }
